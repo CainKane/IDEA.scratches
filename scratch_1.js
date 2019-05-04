bebdbeba43c4914123f5790c486ac3dc5d9d7480
Cain <18299687659@163.com>
2019-05-05 02:30:03 +0800

    5.5-2:30

--- scratch_1.js
+++ scratch_1.js
@@ -1,10 +1,32 @@
-Object.defineProperties(
-    $0 ,
-    {
-        value : {
-            set (val){
-                this.
-            }
-        },
-    },
-);
+//es6 class demo
+
+
+class app {
+    state = {};
+    
+    constructor (
+        a ,
+        b,
+    ) {
+        this.a = a;
+        this.b = b;
+        
+    }
+    
+    cons = () => {
+        console.log(this.a)
+    };
+    
+}
+
+
+class CS extends app {
+    constructor (
+        a ,
+        b,
+    ) {
+        super();
+        this.a = "xcxdsd";
+    }
+    
+}
