c4a7ee965aec0e835c8b0f9693852923e92b9c27
Cain <18299687659@163.com>
2019-05-05 02:30:03 +0800

    5.5-2:30

--- Symbol.js
+++ Symbol.js
@@ -1,14 +1,18 @@
 //所有Symbol变量放在这
 class SYMBOL {
-    
-    addSetter = function (item){
-        if(this[item]){
+    addSetter (item) {
+        if( this[item] ) {
+            console.info("早已存在相同的key,请直接使用或更换其他key");
             return "早已存在相同的key,请直接使用或更换其他key";
         }
         this[item] = Symbol();
-    }.bind(this);
+    }
+    del (item){
+        delete this[item];
+    }
 }
 
 const instence = new SYMBOL();
 
-export {instence as symbol};
+export { instence as symbol };
+export default instence;
